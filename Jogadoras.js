2d511ad2c206b4cec60c56e7ac52175b64ed2117
Bruna Sadi Duarte <101605299+brunasadi@users.noreply.github.com>
2025-09-28 19:34:53 -0300

    Update Jogadoras.js

--- Jogadoras.js
+++ Jogadoras.js
@@ -1,33 +1,36 @@
-export const jogadoras = [
-  {
-    id: 1,
-    nome: "Ana Silva",
-    idade: 20,
-    posicao: "Atacante",
-    clube: "Palmeiras",
-    email: "ana.silva@gmail.com",
-    senha:"123456",
-    foto:"/jogadora.png"  },
-  {
-    id: 2,
-    nome: "Mariana Souza",
-    idade: 22,
-    posicao: "Zagueira",
-    clube: "Corinthians",
-    email: "mariana.souza@gmail.com",
-    senha:"123456",
-    foto:"/j2.png"    
-  },
-  {
-    id: 3,
-    nome: "Carla Mendes",
-    idade: 19,
-    posicao: "Meio-campo",
-    clube: "São Paulo",
-    email: "carla.mendes@gmail.com",
-    senha:"123456",
-    foto:"./j3.png"
-  }
-];
+export default function handler(req, res) {
+  const jogadoras = [
+    {
+      id: 1,
+      nome: "Ana Silva",
+      idade: 20,
+      posicao: "Atacante",
+      clube: "Palmeiras",
+      email: "ana.silva@gmail.com",
+      senha: "123456",
+      foto: "/jogadora.png"
+    },
+    {
+      id: 2,
+      nome: "Mariana Souza",
+      idade: 22,
+      posicao: "Zagueira",
+      clube: "Corinthians",
+      email: "mariana.souza@gmail.com",
+      senha: "123456",
+      foto: "/j2.png"
+    },
+    {
+      id: 3,
+      nome: "Carla Mendes",
+      idade: 19,
+      posicao: "Meio-campo",
+      clube: "São Paulo",
+      email: "carla.mendes@gmail.com",
+      senha: "123456",
+      foto: "/j3.png"
+    }
+  ];
 
-
+  res.status(200).json(jogadoras);
+}
